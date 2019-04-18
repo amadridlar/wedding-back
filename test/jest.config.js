8115aa5790d5c2cd8dfccffd808088eb5686ba16
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-04-18 20:46:31 +0200

    style(jest.config): lint jest.config

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-    "verbose": true,
-  //uncomment the next line to get the coverage report
-  //  "collectCoverage": true,
-    "collectCoverageFrom": [
-          "src/**/*.js",
-          "!**/node_modules/**",
-          ],
-    "coverageDirectory":"test/reports/coverage",
-    "testMatch": [
-        "test/unit/**/*.spec.js"
-    ]
-  };
+  verbose: true,
+  testMatch: [
+    '**/?(*.)+(spec).js?(x)',
+  ],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!**/node_modules/**',
+  ],
+  coverageDirectory: 'test/reports/coverage',
+};
