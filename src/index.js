7f6f672a542f25185c8f38b7abd9e4fd1d4e6971
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-04-19 12:20:09 +0200

    chore(index): body-parser and morgan added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 require('colors');
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const guestsRoutes = require('./routes/gestsRoutes');
 
 const port = '3000';
 const app = express();
+
+// MIDLEWARES
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES
+app.use('/guests', guestsRoutes);
 
 app.get('/', (req, res) => {
   res.status(200);
