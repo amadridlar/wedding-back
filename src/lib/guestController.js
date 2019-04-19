2afe179bdc66e73b460a8c730d7e36bdcc03ffb3
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-04-19 21:14:57 +0200

    chore(guestController): newGuest service adapted to database integration

--- src/lib/guestController.js
+++ src/lib/guestController.js
@@ -1,14 +1,22 @@
+const GuestModel = require('../models/guestModel');
+
 const controller = {
   newGuest: (req, res) => {
-    const responseBody = {
-      id: 'dataBaseId',
-      name: req.body.name,
-      lastname: req.body.lastname,
-      menu: req.body.menu,
-      bus: req.body.bus,
-    };
-    res.status(200);
-    res.json(responseBody);
+    const guest = new GuestModel();
+    guest.name = req.body.name;
+    guest.lastname = req.body.lastname;
+    guest.menu = req.body.menu;
+    guest.bus = req.body.bus;
+
+    guest.save((err) => {
+      if (err) {
+        res.status(500);
+        res.json({ status: 'error', message: 'database error' });
+      } else {
+        res.status(200);
+        res.json({ status: 'success', message: 'guest saved', data: guest });
+      }
+    });
   },
 };
 
