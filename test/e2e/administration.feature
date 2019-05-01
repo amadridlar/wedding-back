61b0fc0f8aea009726f12435279dc6d6a00f57e6
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-05-01 22:44:46 +0200

    test(administration feature): bdd for admin actions

--- test/e2e/administration.feature
+++ test/e2e/administration.feature
@@ -4,6 +4,7 @@ Feature: Administration actions
   I want to manage guest data
   So that get full controll of guest list
 
+  @Definition
   Scenario Outline: Add a new guest
     Given new guest
       | Name     | <name>     |
@@ -16,17 +17,17 @@ Feature: Administration actions
       | <name> | <lastname> | <menu> | <bus> |
 
     Examples:
-      | name    | lastname | menu | bus   |
-      | 'Johny' | 'Reland' | 1    | true  |
-      | 'Johny' | ''       | 2    | false |
+      | name  | lastname | menu | bus   |
+      | Johny | Reland   | 1    | true  |
+      | Johny |          | 2    | false |
 
   Scenario: Remove a guest
-    Given a guest storaged
+    Given a guest already storaged
     When I remove the guest
     Then the guest is removed correctly
 
-  Scenario Outline: Scenario Outline name: Modify guest information
-    Given a guest storaged
+  Scenario Outline: Modify guest information
+    Given a guest already storaged
     When I modify some guest data
       | Name     | <name>     |
       | Lastname | <lastname> |
@@ -39,11 +40,25 @@ Feature: Administration actions
       | Bus      | <bus>      |
 
     Examples:
-      | name    | lastname | menu | bus  |
-      | 'Johny' | 'Jeremy' | 1    | true |
+      | name  | lastname | menu | bus  |
+      | Johny | Jeremy   | 1    | true |
 
   Scenario: Add a guest already storaged
     Given a guest already storaged
     When I add the guest
-    Then application inform me that the user is already storaged
+    Then application informs me that the user is already storaged
     And the guest is not storaged
+
+  Scenario: Trying to remove a guest who is not storaged
+    Given a guest not storaged
+    When I remove the guest
+    Then the application informs that the guest does not exist
+
+  Scenario: Trying to modify a guest who is not storaged
+    Given a guest not storaged
+    When I modify some guest data
+      | Name     | <name>     |
+      | Lastname | <lastname> |
+      | Menu     | <menu>     |
+      | Bus      | <bus>      |
+    Then the application informs that the user does not exist
--- test/e2e/steps/administration.steps.js
+++ test/e2e/steps/administration.steps.js
@@ -1,45 +1,46 @@
-  const {Given, When, Then} = require('cucumber');
-  
-  Given(/^new guest$/,  (table) => {
-    return 'pending';
-  });
+const { Given, When, Then, Before } = require('cucumber');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 
-  Given(/^a guest storaged$/,  () => {
-    return 'pending';
-  });
+const expect = chai.expect;
+chai.use(chaiHttp);
 
-  Given(/^a guest already storaged$/,  () => {
-    return 'pending';
-  });
+let guest = {};
+let guestId = '';
+const serverUrl = 'http://localhost:3000';
 
-  When(/^I add the guest$/,  () => {
-    return 'pending';
-  });
+Before(function () {
+  guest = {};
+  guestId = '';
+});
 
-  When(/^I remove the guest$/,  () => {
-    return 'pending';
-  });
+Given('new guest', function (dataTable) {
+  guest = dataTable.rowsHash();
+});
 
-  When(/^I modify some guest data$/,  () => {
-    return 'pending';
-  });
+When('I add the guest', function (done) {
+  return chai.request(serverUrl)
+    .post('/back/guest/new')
+    .set('Content-Type', 'application/json')
+    .send(guest)
+    .end(function (err, res) {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res.body).to.have.property('_id');
+      expect(res.body.data).to.be.equal(guest);
+      guestId = res.body._id;
+      done();
+    })
+});
 
-  Then(/^the guest is storaged correctly$/,  () => {
-    return 'pending';
-  });
-
-  Then(/^the guest is removed correctly$/,  () => {
-    return 'pending';
-  });
-
-  Then(/^the new data modified is storaged correctly$/,  () => {
-    return 'pending';
-  });
-
-  Then(/^application inform me that the user is already storaged$/,  () => {
-    return 'pending';
-  });
-
-  Then(/^the guest is not storaged$/,  () => {
-    return 'pending';
-  });
+Then('the guest is storaged correctly', function (dataTable, done) {
+  const expectedGuest = dataTable.rowsHash();
+  return chai.request(serverUrl)
+    .get(`/back/guest/${guestId}`)
+    .end(function(err, res){
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect(res.body.data).to.be.deep.equal(expectedGuest);
+      done();
+    })
+});
