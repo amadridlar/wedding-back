42e27f8f6bb9c29c6c1aad349915ef579764204d
Álvaro Madrid Lara <alvaro.madrid.lara.contractor@bbva.com>
2019-05-04 14:10:47 +0200

    chore(jest.config): move file to root dir

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
 module.exports = {
+  automock: false,
   verbose: true,
   testEnvironment: 'node',
   testMatch: [
     '**/?(*.)+(test).js?(x)',
   ],
   collectCoverageFrom: [
-    'src/**/*.js',
-    '!**/node_modules/**',
+    '<rootDir>/src/**/*.js',
+  ],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/src/database/models/',
+    '<rootDir>/src/index.js',
   ],
   coverageDirectory: 'test/reports/coverage',
 };
